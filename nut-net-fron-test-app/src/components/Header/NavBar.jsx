c1214eb2a5a7ebb39b817e598b84b7433e750170
Sgadmer <r.smim@yandex.ru>
2020-10-11 18:36:33 +0400

    1) Написал адаптив под все блоки 2) Написал излишние дополнения в коде, которые нужны для корректного адаптива в IE

--- nut-net-fron-test-app/src/components/Header/NavBar.jsx
+++ nut-net-fron-test-app/src/components/Header/NavBar.jsx
@@ -1,45 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../tajamLogo.png";
 
 function NavBar() {
-    return (
+  const [navigationClasses, setNavigationClasses] = useState("navigation");
+  const [navbarToggleBTNClasses, setNavbarToggleBTNClasses] = useState(
+    "menu-icon"
+  );
 
-        <nav className="navBar  contentWrapper">
-          <a href="/#" className="navBar__logo">
-            <img src={logo} alt="HeaderLogo" />
+  function toggleNavigation() {
+    let BTNclasses = navbarToggleBTNClasses;
+    let navClasses;
+    if (BTNclasses.match("clicked")) {
+      BTNclasses = "menu-icon";
+      navClasses = "navigation";
+    } else {
+      BTNclasses += " clicked";
+      navClasses = "navigation navigation__showed";
+    }
+
+    setNavigationClasses(navClasses);
+    setNavbarToggleBTNClasses(BTNclasses);
+  }
+
+  return (
+    <>
+      <nav className="navBar  contentWrapper">
+        <a href="/1" className="navBar__logo">
+          <img src={logo} alt="HeaderLogo" />
+        </a>
+
+        <div className={navigationClasses}>
+          <a href="/2" className="navigation__btn_active">
+            HOME
           </a>
+          <a href="/3" className="navigation__btn">
+            ABOUT
+          </a>
+          <a href="/4" className="navigation__btn">
+            EXPERTISE
+          </a>
+          <a href="/5" className="navigation__btn">
+            TEAMS
+          </a>
+          <a href="/6" className="navigation__btn">
+            WORKS
+          </a>
+          <a href="/7" className="navigation__btn">
+            PEOPLE SAY
+          </a>
+          <a href="/8" className="navigation__btn">
+            CONTACT
+          </a>
+        </div>
 
-          <div className="navigation">
-            <a href="/#" className="navigation__btn_active">
-              HOME
-            </a>
-            <a href="/#" className="navigation__btn">
-              ABOUT
-            </a>
-            <a href="/#" className="navigation__btn">
-              EXPERTISE
-            </a>
-            <a href="/#" className="navigation__btn">
-              TEAMS
-            </a>
-            <a href="/#" className="navigation__btn">
-              WORKS
-            </a>
-            <a href="/#" className="navigation__btn">
-              PEOPLE SAY
-            </a>
-            <a href="/#" className="navigation__btn">
-              CONTACT
-            </a>
+        <button
+          className="navbarToggle-BTN"
+          type="button"
+          onClick={() => toggleNavigation()}
+        >
+          <div className={navbarToggleBTNClasses}>
+            <div className="dot"></div>
+            <div className="dot"></div>
+            <div className="dot"></div>
+            <div className="dot"></div>
+            <div className="dot"></div>
+            <div className="dot"></div>
+            <div className="dot"></div>
+            <div className="dot"></div>
+            <div className="dot"></div>
           </div>
-        </nav>
-        
-
-    );
+        </button>
+      </nav>
+    </>
+  );
 }
 
 export default NavBar;
-
-
-
-
